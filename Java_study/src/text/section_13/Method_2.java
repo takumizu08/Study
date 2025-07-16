26cfdb3bd1b90bb65df0c7502f079a49c1bc90b2
TakumuMizuhara <131280795+takumizu08@users.noreply.github.com>
2025-07-17 06:46:59 +0900

    Mesthod　study

---
+++ Java_study/src/text/section_13/Method_2.java
@@ -0,0 +1,18 @@
+package text.section_13;
+
+public class Method_2 {
+
+	public static void main(String[] args) {
+		int greeting = 123;
+		
+		greeting();
+		System.out.println(greeting);
+		greeting();
+	}
+	public static void greeting() {
+		System.out.println("おはようございます！");
+        System.out.println("昨日はよく眠れましたか？");
+        System.out.println("今日も一日頑張りましょう！");
+	}
+
+}
